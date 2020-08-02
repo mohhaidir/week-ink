0b555b1f607639c3f9bdc6d25ea61de3b75d9bdc
mohhaidir <khoiruljmbr@gmail.com>
2020-08-02 09:37:30 +0700

    success display value of fetch

--- reacto/src/components/Testimonial.js
+++ reacto/src/components/Testimonial.js
@@ -3,6 +3,7 @@ import React from "react";
 export default function Testimonial(props) {
   return (
     <>
+      {JSON.stringify(props)}
       <div>
         <div>
           <div className="testimonial-wrapper-1">
@@ -12,6 +13,14 @@ export default function Testimonial(props) {
             </div>
           </div>
           <div className="testimonials-text-slider">
+            <div>
+              {props.testimonials.map(testimony => (
+                <div>
+                  <h4>{testimony.by}</h4>
+                  <p>{testimony.testimony}</p>
+                </div>
+              ))}
+            </div>
             <p> -should display fetch data of testimonials-</p>
           </div>
         </div>
